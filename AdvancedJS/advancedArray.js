c172cd9bebae40b64152ae4909e4adc70754b21d
FURKAN <furkan199905@gmail.com>
2022-05-15 19:40:25 +0300

    added some examples

--- AdvancedJS/advancedArray.js
+++ AdvancedJS/advancedArray.js
@@ -1,13 +1,3 @@
-// forEach
-/*
-const arr = [5, 6, 8];
-const newArr = arr;
-
-arr.forEach((num) => {
-  newArr.push(num * 2);
-});
-console.log(newArr);
-*/
 const profiles = [
   {
     username: "john",
@@ -34,7 +24,14 @@ const profiles = [
     items: ["book", "pen"],
   },
 ];
-//forEach
+
+const urunler = [
+  { id: 1, name: "Kalem", fiyat: 5 },
+  { id: 2, name: "Defter", fiyat: 10 },
+  { id: 3, name: "Silgi", fiyat: 2 },
+  { id: 4, name: "Kalemtras", fiyat: 7 },
+];
+//forEach with arrays
 let newUsernames = [];
 profiles.forEach(({ username }) => {
   username = username + "!";
@@ -42,19 +39,22 @@ profiles.forEach(({ username }) => {
 });
 console.log(newUsernames);
 
-/*  we can do in html like that
-profiles.forEach((par, index) => {
-  ++index;
-  $("#result").innerHtml += index;
-  ".Eleman" + par;
-  ("<br>");
-});
-*/
 // Map
+//It applies the entered callback function to the given array elements and creates a new array with the results obtained.
 let newProfiles = profiles.map(({ username }) => {
   return username + "?";
 });
 console.log(newProfiles);
+
+//Map 2 in react component with prop
+
+function urunGoster(urunListesi) {
+  return urunListesi.map((urun) => (
+    <li key={urun.id}>
+      {urun.name} fiyati {urun.fiyat} TL
+    </li>
+  ));
+}
 
 // filter
 let newTeam = profiles.filter(({ team }) => team === "red");
@@ -65,3 +65,18 @@ const total = profiles.reduce((acc, user) => {
   return acc + user.score;
 }, 0);
 console.log(total);
+
+// reduce 2
+const toplamUrunFiyati = urunler.reduce(
+  (toplam, urun) => (toplam = toplam + urun.fiyat),
+  0
+);
+
+console.log(toplamUrunFiyati);
+
+const toplamString = urunler.reduce(
+  (toplam, urun) => (toplam = `${toplam} ${urun.name}`),
+  "Urun Isimleri:"
+);
+
+console.log(toplamString);
--- EcmaScripts/es6.js
+++ EcmaScripts/es6.js
@@ -1,19 +1,20 @@
 // let is block scope. We can use "let" inside of {} where it create
 let bodyWeight = 70;
 
-function waterCalc(){
-    let age = 22;
-    if(bodyWeight > 69){
+function waterCalc() {
+  let age = 22;
+  if (bodyWeight > 69) {
     let water = bodyWeight / 20;
-    console.log(`your body weight is ${bodyWeight}kg , your age is ${age} and you have to drink ${water} L `);
-}
+    console.log(
+      `your body weight is ${bodyWeight}kg , your age is ${age} and you have to drink ${water} L `
+    );
+  }
 }
 waterCalc();
-// console.log(water);  Uncaught ReferenceError: water is not defined 
 
 // In object ve can change values and ve can add attribute in object
-const car = {type:"Fiat", model:"Egea", color:"white"};
-car.color = "red"; // we can change 
+const car = { type: "Fiat", model: "Egea", color: "white" };
+car.color = "red"; // we can change
 car.fuel = "diesel";
 
 const COLORS = ["red", "green", "blue"];
@@ -23,22 +24,22 @@ console.log(COLORS[0]); // yellow
 
 // Destructuring
 const person = {
-    firstName : "John",
-    lastName  : "Doe",
-    age       : 50,
-    eyeColor  : "blue"
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
 };
 
 const { firstName, lastName, age, eyeColor } = person;
 
-console.log(lastName) //Doe
+console.log(lastName); //Doe
 
 // Object properties
-const a = 'test';
+const a = "test";
 const b = true;
 const c = 789;
 
-const okObj = {a, b, c};
+const okObj = { a, b, c };
 
 // Template strings
 let city = "Ankara";
@@ -47,9 +48,9 @@ console.log(message);
 
 // Arrow Function
 const whereAmI = (username, location) => {
-    if (username && location) {
-        return "I am not lost";
-    } else {
-        return "I am totally lost!";
-    }
-}
+  if (username && location) {
+    return "I am not lost";
+  } else {
+    return "I am totally lost!";
+  }
+};
